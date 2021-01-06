2b7439063a3571fae44711bea28207b1f2f8c4fe
isaiahaiasi <computerblender@gmail.com>
2021-01-06 10:38:15 -0700

    Remove unnecessary comments; Reformat and clarify comments

--- src/get_urls.py
+++ src/get_urls.py
@@ -2,26 +2,21 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# NEED TO LOOK UP HOW TO PROPERLY ORGANIZE PY FILES :/
-# Something like "if __name__ == __main__"...
-# Currently, code to execute is below function
+# TODO: Create main function
 
-# TODO: return a DataFrame with a row for each url (currently just returns an array of urls)
-# columns: url, dataColumns (default: "name")
-# args:
+# get_urls
+# Returns array of string arrays containing: url, name, sector
+# Args:
 #   UrlSource: the webpage to pull the table from
 #   UrlBase: the links are partial in this case, so append them to a base url
 #   UrlContains: a way to specify which links I want, to exclude garbage. Default is blank
 #   DataColumns: the name of columns to add to the dataframe (default is "name")
-#       (unfortunately, don't think I'll be able to make much use of this, bc it only has most recent data, & I want every year)
 def get_urls(urlSource, urlBase, urlContains = "", dataColumns = ["name"]):
   response = requests.get(urlSource)
   soup = BeautifulSoup(response.text, "html.parser")
   tables = soup.find_all("table")
-  # tableDF = pd.read_html(str(tables[1]),header=0) # use first tr as headers
-  # print(tableDF)
   
-  urls = [] # instead of a 1d array, I should make a table & add any optional columns (at the very least, including "name")
+  urls = []
   dataColumns = ["url"] + dataColumns
   df = pd.DataFrame(columns=dataColumns)
 
@@ -30,20 +25,18 @@ def get_urls(urlSource, urlBase, urlContains = "", dataColumns = ["name"]):
     url = ["","",""]
     linkRow = False
 
+    # If there's a link, add it to the urls list
     for cell in tds:
-      #if there's a link, add it to the urls list
       urlTail = get_link(cell, urlContains)
       if (urlTail != ""):
         url[0] = urlBase + urlTail
         linkRow = True
 
+      # Copy the cells I care about from the tableDF into the df for export
       if linkRow is True:
         url[1] = tds[0].text
         url[2] = tds[1].text
         urls.append(url)
-  #copy the cells I care about from the tableDF into the df for export
-
-  #print("number of filtered urls: " + str(len(filteredUrls)) + ". eg: " + filteredUrls[0])
   
   return urls
 
@@ -58,16 +51,18 @@ def get_link(element, urlContains):
 
   except:
     pass
-    #ISSUE
-    #TypeError: 'NoneType' object is not subscriptable 
-    # (I'm pretty sure the stackoverflow I stole this from added this trycatch to hide the fact that their code is borked. 
-    # It's 2am, so I'll figure it out later)
+    # ISSUE
+    # TypeError: 'NoneType' object is not subscriptable 
+    # Blame StackOverflow
   
   return link
 
+# url_to_table
+# Returns df of data on a charity
+# Args:
+#   urlData: string array of [url, name, sector]
 def url_to_table(urlData):
-  # the fields I'm interested in
-  # (which should just be a list in a json/txt file or something)
+  # TODO: Pull from options file
   fields = [  
     "net assets", 
     "total revenue", 
@@ -79,36 +74,32 @@ def url_to_table(urlData):
     "total assets",
     ]
   fields = set(fields)
-    
-  #getting dataframe of table from url could be its own function, I just don't know how to share functions across files yet
+
   response = requests.get(urlData[0])
   soup = BeautifulSoup(response.text, "html.parser")
   tables = soup.find_all("table")
-    
-  if len(tables) < 4: # Some pages don't have any tables/data :(
+  
+  # Don't try to grab data from pages w/o full dataset
+  if len(tables) < 4:
     return None
 
-  htmlDF = pd.read_html(str(tables))[3] # use second tr as headers
+  htmlDF = pd.read_html(str(tables))[3]
     
   returnDF = pd.DataFrame([["Name", "Sector"]], columns=["_Name", "_Sector"])
   for i in range(2, len(htmlDF.index - 1)):
-    # see if loc[i][0] matches any field
     if str(htmlDF.loc[i][0]).lower() not in fields:
       # print(str(htmlDF.loc[i][0]) + " not a field")
       continue
     else:
       transposedDF = htmlDF.iloc[i].T
       returnDF = pd.concat([returnDF, transposedDF], ignore_index=True, axis=1)
-      # print("found " + str(htmlDF.loc[i][0]))
-
     
-  #make the first row the headers
+  # Make the first row the headers
   dfHeads = returnDF.iloc[0]
   returnDF = returnDF[1:]
   returnDF.columns = dfHeads
 
-  #once I have the data populated from the crawl, 
-  #fill in the name/sector column for every row
+  # Fill in the name/sector column for every row
   returnDF.loc[:,"Name"] = urlData[1]
   returnDF.loc[:,"Sector"] = urlData[2]
 
@@ -116,7 +107,9 @@ def url_to_table(urlData):
 
 sourceURL = "https://briinstitute.com/mw/compare.php"
 baseURL = "https://briinstitute.com/mw/"
-urlContains = "ein" #only grab urls that have "ein" in the title (the format url I'm looking for is "ministry.php?ein=########")
+
+# Only grab urls with "ein" in the title (desired url format: "ministry.php?ein=########")
+urlContains = "ein"
 myURLS = get_urls(sourceURL, baseURL, urlContains)
 
 print("length of url set: " + str(len(myURLS)))
