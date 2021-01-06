2d17943b72a38d9261baced6b6a0d46338afc78f
isaiahaiasi <computerblender@gmail.com>
2021-01-05 23:48:03 -0700

    Tabbing correction, changed to 2sp

--- get_urls.py
+++ get_urls.py
@@ -15,102 +15,104 @@ from bs4 import BeautifulSoup
 #   DataColumns: the name of columns to add to the dataframe (default is "name")
 #       (unfortunately, don't think I'll be able to make much use of this, bc it only has most recent data, & I want every year)
 def get_urls(urlSource, urlBase, urlContains = "", dataColumns = ["name"]):
-    response = requests.get(urlSource)
-    soup = BeautifulSoup(response.text, "html.parser")
-    tables = soup.find_all("table")
-    # tableDF = pd.read_html(str(tables[1]),header=0) # use first tr as headers
-    # print(tableDF)
-    
-    urls = [] # instead of a 1d array, I should make a table & add any optional columns (at the very least, including "name")
-    dataColumns = ["url"] + dataColumns
-    df = pd.DataFrame(columns=dataColumns)
+  response = requests.get(urlSource)
+  soup = BeautifulSoup(response.text, "html.parser")
+  tables = soup.find_all("table")
+  # tableDF = pd.read_html(str(tables[1]),header=0) # use first tr as headers
+  # print(tableDF)
+  
+  urls = [] # instead of a 1d array, I should make a table & add any optional columns (at the very least, including "name")
+  dataColumns = ["url"] + dataColumns
+  df = pd.DataFrame(columns=dataColumns)
 
-    for tr in tables[1].findAll("tr"):
-        tds = tr.findAll("td")
-        url = ["","",""]
-        linkRow = False
+  for tr in tables[1].findAll("tr"):
+    tds = tr.findAll("td")
+    url = ["","",""]
+    linkRow = False
 
-        for cell in tds:
-            #if there's a link, add it to the urls list
-            urlTail = get_link(cell, urlContains)
-            if (urlTail != ""):
-                url[0] = urlBase + urlTail
-                linkRow = True
+    for cell in tds:
+      #if there's a link, add it to the urls list
+      urlTail = get_link(cell, urlContains)
+      if (urlTail != ""):
+        url[0] = urlBase + urlTail
+        linkRow = True
 
-        if linkRow is True:
-            url[1] = tds[0].text
-            url[2] = tds[1].text
-            urls.append(url)
-    #copy the cells I care about from the tableDF into the df for export
+      if linkRow is True:
+        url[1] = tds[0].text
+        url[2] = tds[1].text
+        urls.append(url)
+  #copy the cells I care about from the tableDF into the df for export
 
-    #print("number of filtered urls: " + str(len(filteredUrls)) + ". eg: " + filteredUrls[0])
-    
-    return urls
+  #print("number of filtered urls: " + str(len(filteredUrls)) + ". eg: " + filteredUrls[0])
+  
+  return urls
 
 def get_link(element, urlContains):
-    link = ""
+  link = ""
 
-    try:
-        link = element.find('a')['href']
+  try:
+    link = element.find('a')['href']
 
-        if urlContains != "" and not (urlContains in link):
-            print("invalid link filtered")
+    if urlContains != "" and not (urlContains in link):
+      print("invalid link filtered")
 
-    except:
-        pass
-        #ISSUE
-        #TypeError: 'NoneType' object is not subscriptable 
-        # (I'm pretty sure the stackoverflow I stole this from added this trycatch to hide the fact that their code is borked. 
-        # It's 2am, so I'll figure it out later)
-    
-    return link
+  except:
+    pass
+    #ISSUE
+    #TypeError: 'NoneType' object is not subscriptable 
+    # (I'm pretty sure the stackoverflow I stole this from added this trycatch to hide the fact that their code is borked. 
+    # It's 2am, so I'll figure it out later)
+  
+  return link
 
 def url_to_table(urlData):
-    # the fields I'm interested in
-    # (which should just be a list in a json/txt file or something)
-    fields = [  "net assets", 
-                "total revenue", 
-                "total contributions", 
-                "total expenses",
-                "fundraising",
-                "program services",
-                "total current assets",
-                "total assets",]
-    fields = set(fields)
+  # the fields I'm interested in
+  # (which should just be a list in a json/txt file or something)
+  fields = [  
+    "net assets", 
+    "total revenue", 
+    "total contributions", 
+    "total expenses",
+    "fundraising",
+    "program services",
+    "total current assets",
+    "total assets",
+    ]
+  fields = set(fields)
     
-    #getting dataframe of table from url could be its own function, I just don't know how to share functions across files yet
-    response = requests.get(urlData[0])
-    soup = BeautifulSoup(response.text, "html.parser")
-    tables = soup.find_all("table")
+  #getting dataframe of table from url could be its own function, I just don't know how to share functions across files yet
+  response = requests.get(urlData[0])
+  soup = BeautifulSoup(response.text, "html.parser")
+  tables = soup.find_all("table")
     
-    if len(tables) < 4: # Some pages don't have any tables/data :(
-        return None
+  if len(tables) < 4: # Some pages don't have any tables/data :(
+    return None
 
-    htmlDF = pd.read_html(str(tables))[3] # use second tr as headers
+  htmlDF = pd.read_html(str(tables))[3] # use second tr as headers
     
-    returnDF = pd.DataFrame([["Name", "Sector"]], columns=["_Name", "_Sector"])
-    for i in range(2, len(htmlDF.index - 1)):
-        # see if loc[i][0] matches any field
-        if str(htmlDF.loc[i][0]).lower() not in fields:
-            # print(str(htmlDF.loc[i][0]) + " not a field")
-            continue
-        else:
-            transposedDF = htmlDF.iloc[i].T
-            returnDF = pd.concat([returnDF, transposedDF], ignore_index=True, axis=1)
-            # print("found " + str(htmlDF.loc[i][0]))
+  returnDF = pd.DataFrame([["Name", "Sector"]], columns=["_Name", "_Sector"])
+  for i in range(2, len(htmlDF.index - 1)):
+    # see if loc[i][0] matches any field
+    if str(htmlDF.loc[i][0]).lower() not in fields:
+      # print(str(htmlDF.loc[i][0]) + " not a field")
+      continue
+    else:
+      transposedDF = htmlDF.iloc[i].T
+      returnDF = pd.concat([returnDF, transposedDF], ignore_index=True, axis=1)
+      # print("found " + str(htmlDF.loc[i][0]))
 
     
-    #make the first row the headers
-    dfHeads = returnDF.iloc[0]
-    returnDF = returnDF[1:]
-    returnDF.columns = dfHeads
+  #make the first row the headers
+  dfHeads = returnDF.iloc[0]
+  returnDF = returnDF[1:]
+  returnDF.columns = dfHeads
 
-    #once I have the data populated from the crawl, 
-    #fill in the name/sector column for every row
-    returnDF.loc[:,"Name"] = urlData[1]
-    returnDF.loc[:,"Sector"] = urlData[2]
+  #once I have the data populated from the crawl, 
+  #fill in the name/sector column for every row
+  returnDF.loc[:,"Name"] = urlData[1]
+  returnDF.loc[:,"Sector"] = urlData[2]
 
-    return returnDF
+  return returnDF
 
 sourceURL = "https://briinstitute.com/mw/compare.php"
 baseURL = "https://briinstitute.com/mw/"
@@ -121,11 +123,11 @@ print("length of url set: " + str(len(myURLS)))
 df = url_to_table(myURLS[0])
 i = 1
 for url in myURLS[1:]:
-    i = i + 1
-    print(str(i))
-    df = pd.concat([df, url_to_table(url)])
+  i = i + 1
+  print(str(i))
+  df = pd.concat([df, url_to_table(url)])
 
 with pd.ExcelWriter("test-excel.xlsx") as writer: # pylint: disable=abstract-class-instantiated
-    df.to_excel(writer, sheet_name="Sheet1")
+  df.to_excel(writer, sheet_name="Sheet1")
 
 print("number of urls: " + str(len(myURLS)))
