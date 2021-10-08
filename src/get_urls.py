7e7da2c2fc4591e5db7a88cb380b162e576dde83
isaiahaiasi <isaiah.althouse@gmail.com>
2021-10-08 10:48:22 -0700

    Format get_urls and refactor to use csv methods
    CSV generation was borked because delimiters weren't escaped

--- src/get_urls.py
+++ src/get_urls.py
@@ -1,8 +1,7 @@
-import pandas as pd
 import requests
+import os
+import csv
 from bs4 import BeautifulSoup
-
-# TODO: Create main function
 
 # get_urls
 # Returns array of string arrays containing: url, name, sector
@@ -11,64 +10,70 @@ from bs4 import BeautifulSoup
 #   UrlBase: the links are partial in this case, so append them to a base url
 #   UrlContains: a way to specify which links I want, to exclude garbage. Default is blank
 #   DataColumns: the name of columns to add to the dataframe (default is "name")
-def get_urls(urlSource, urlBase, urlContains = "", dataColumns = ["name"]):
-  response = requests.get(urlSource)
-  soup = BeautifulSoup(response.text, "html.parser")
-  tables = soup.find_all("table")
 
-  urls = []
-  dataColumns = ["url"] + dataColumns
-  df = pd.DataFrame(columns=dataColumns)
 
-  for tr in tables[0].findAll("tr"):
-    tds = tr.findAll("td")
-    url = ["","",""]
+def get_urls(urlSource, urlBase, urlContains="", dataColumns=["name"]):
+    response = requests.get(urlSource)
+    soup = BeautifulSoup(response.text, "html.parser")
+    tables = soup.find_all("table")
 
-    urlTail = get_link(tds[0], urlContains)
+    urls = []
+    dataColumns = ["url"] + dataColumns
 
-    # If there's a link, add it & the tds I care about to the urls list
-    if (urlTail != ""):
-      url[0] = urlBase + urlTail
-      url[1] = tds[0].text
-      url[2] = tds[1].text
-      urls.append(url)
+    for tr in tables[0].findAll("tr"):
+        tds = tr.findAll("td")
+        url = ["", "", ""]
 
-  return urls
+        urlTail = get_link(tds[0], urlContains)
+
+        # If there's a link, add it & the tds I care about to the urls list
+        if (urlTail != ""):
+            url[0] = urlBase + urlTail
+            url[1] = tds[0].text
+            url[2] = tds[1].text
+            urls.append(url)
+
+    return urls
+
 
 def get_link(element, urlContains):
-  link = ""
+    link = ""
 
-  try:
-    link = element.find('a')['href']
+    try:
+        link = element.find('a')['href']
 
-    if urlContains != "" and not (urlContains in link):
-      print("invalid link filtered")
+        if urlContains != "" and not (urlContains in link):
+            print("invalid link filtered")
 
-  except:
-    pass
-    # ISSUE
-    # TypeError: 'NoneType' object is not subscriptable
-    # Blame StackOverflow
+    except:
+        pass
+        # ISSUE
+        # TypeError: 'NoneType' object is not subscriptable
+        # Blame StackOverflow
 
-  return link
+    return link
+
 
 def save_urls(urls):
-  f = open("data/urls_data.csv", "w")
+    # make sure directory exists
+    if not os.path.exists("data"):
+        os.mkdir("data")
 
-  for url in urls:
-    f.write(url[0] + "," + url[1] + "," + url[2] + "\n")
-
-  f.close()
+    # write urls data to csv
+    with open("data/urls_data.csv", "w") as f:
+        csvwriter = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerows(urls)
 
 
 def main():
-  # TODO: for real, add these to settings...
-  sourceURL = "https://briinstitute.com/mw/compare.php"
-  baseURL = "https://briinstitute.com/mw/"
-  urlContains = "ein"
+    # TODO: for real, add these to settings...
+    sourceURL = "https://briinstitute.com/mw/compare.php"
+    baseURL = "https://briinstitute.com/mw/"
+    urlContains = "ein"
 
-  urls = get_urls(sourceURL, baseURL, urlContains)
-  save_urls(urls)
+    urls = get_urls(sourceURL, baseURL, urlContains)
+    save_urls(urls)
+
 
 if __name__ == "__main__":
-  main()
+    main()
